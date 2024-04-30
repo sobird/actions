bca6652b4673524d7d134942dd368fc52d6221d3
sobird <i@sobird.me>
2024-04-30 11:38:08 +0800

    chore: update artifact server

--- pkg/artifacts/cache/server.ts
+++ pkg/artifacts/cache/server.ts
@@ -1,10 +1,24 @@
+/**
+ * artifact cache server
+ * Spin up a local Github artifact cache server to be used with act Github actions that uses actions/cache
+ *
+ * @see https://github.com/sp-ricard-valverde/github-act-cache-server
+ * @see https://github.com/JEFuller/artifact-server/blob/main/index.js
+ * sobird<i@sobird.me> at 2024/04/30 1:58:31 created.
+ */
+
 import os from 'os';
 import fs from 'fs';
 import path from 'path';
+import type { AddressInfo } from 'net';
 import log4js, { Logger } from 'log4js';
 import ip from 'ip';
+import express, { Handler } from 'express';
+import { ClassicLevel } from 'classic-level';
 
-class ArtifactCacheServer {
+const BASE_URL = '/_apis/artifactcache';
+
+class ArtifactsCacheServer {
   constructor(
     public dir: string = path.join(os.homedir(), '.cache', 'actcache'),
     public outboundIP: string = ip.address(),
@@ -14,7 +28,41 @@ class ArtifactCacheServer {
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir, { recursive: true });
     }
+
+    const app = express();
+
+    app.use(this.middleware);
+
+    app.get(path.join(BASE_URL, 'cache'), (req, res) => {
+      // todo
+      console.log('1212', 1212);
+      res.json({
+        ok: 0,
+      });
+    });
+
+    const server = app.listen(port, () => {
+      console.log('服务已经启动, 端口监听为:', (server.address() as AddressInfo).port);
+    });
+  }
+
+  // GET /_apis/artifactcache/cache
+  find: Handler = (req, res) => {
+    //
+  };
+
+  private middleware: Handler = (req, res, next) => {
+    this.logger.debug(`Request method: ${req.method}, Request url: ${req.originalUrl}`);
+    next();
+    this.gcCache();
+  };
+
+  gcCache() {
+    //
+    console.log('gcCache');
   }
 }
 
-export default ArtifactCacheServer;
+export default ArtifactsCacheServer;
+
+const cache = new ArtifactsCacheServer(undefined, undefined, 3000);
--- pkg/artifacts/server.ts
+++ pkg/artifacts/server.ts
@@ -1,140 +1,241 @@
-import http from 'http';
-import fs from 'fs';
-import path from 'path';
+import os from 'node:os';
+import { AddressInfo } from 'node:net';
+import fs from 'node:fs';
+import path from 'node:path';
+import ip from 'ip';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { totalist } from 'totalist/sync';
 
-const BASE_DIR = './artifacts'; // 基础目录，用于存储上传的文件
-const PORT = 3000;
+class ArtifactServer {
+  constructor(
+    public dir: string = path.join(os.homedir(), '.artifacts'),
+    public host: string = ip.address(),
+    public port: number = 0,
+  ) {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    const app = express();
 
-// 创建基础目录
-if (!fs.existsSync(BASE_DIR)) {
-  fs.mkdirSync(BASE_DIR, { recursive: true });
+    app.use(bodyParser.json());
+    app.use(bodyParser.raw({
+      type: 'application/octet-stream',
+      limit: '50mb',
+    }));
+
+    app.get('/', (req, res) => {
+      res.send({
+        status: 'success',
+      });
+    });
+
+    app.post('/_apis/pipelines/workflows/:runId/artifacts', (req, res) => {
+      const { runId } = req.params;
+      const baseURL = `${req.protocol}://${req.get('host')}${req.baseUrl}`;
+
+      res.json({ fileContainerResourceUrl: `${baseURL}/upload/${runId}` });
+    });
+
+    app.patch('/_apis/pipelines/workflows/:runId/artifacts', (req, res) => {
+      const { runId } = req.params;
+
+      res.json({ message: 'success', runId });
+    });
+
+    app.get('/_apis/pipelines/workflows/:runId/artifacts', (req, res) => {
+      const { runId } = req.params;
+      const artifacts = new Set();
+      const baseURL = `${req.protocol}://${req.get('host')}${req.baseUrl}`;
+      const safePath = path.join(this.dir, path.normalize(runId));
+      totalist(safePath, (relPath) => {
+        const name = relPath.replace('\\', '/');
+        const fileDetails = {
+          name: name.split('/')[0],
+          fileContainerResourceUrl: `${baseURL}/download/${runId}`,
+        };
+        artifacts.add(fileDetails);
+      });
+      console.log(artifacts);
+      res.status(200).json({ count: artifacts.size, value: [...artifacts] });
+    });
+
+    app.get('/download/:container', (req, res) => {
+      const { container } = req.params;
+      const { itemPath } = req.query;
+
+      const baseURL = `${req.protocol}://${req.get('host')}${req.baseUrl}`;
+      const safePath = path.join(this.dir, container, itemPath as string);
+
+      const files = new Set();
+      totalist(safePath, (relPath, absPath) => {
+        console.log(relPath);
+        console.log(absPath);
+        files.add({
+          path: path.normalize(relPath),
+          itemType: 'file',
+          contentLocation: `${baseURL}/download/${container}/${relPath.replace('\\', '/')}`,
+        });
+      });
+      res.status(200).json({ value: [...files] });
+    });
+
+    app.get('/download/:container/:path(*)', (req, res) => {
+      const safePath = path.join(this.dir, req.params.container, req.params.path);
+      fs.createReadStream(safePath, { encoding: 'utf-8' }).pipe(res);
+    });
+
+    app.put('/upload/:runId', (req, res) => {
+      const { runId } = req.params;
+      const { itemPath } = req.query;
+      const safePath = path.join(this.dir, runId, itemPath as string);
+      fs.mkdirSync(path.dirname(safePath), { recursive: true });
+
+      // 写入文件
+      const fileWriteStream = fs.createWriteStream(safePath);
+      req.pipe(fileWriteStream);
+
+      // 响应上传成功
+      fileWriteStream.on('close', () => {
+        res.json(JSON.stringify({ message: 'success' }));
+      });
+    });
+
+    const server = app.listen(port, () => {
+      console.log('服务已经启动, 端口监听为:', (server.address() as AddressInfo).port);
+    });
+  }
 }
 
+export default ArtifactServer;
+
+const server = new ArtifactServer(undefined, undefined, 3000);
+console.log('server', server);
 // HTTP 服务器
-const server = http.createServer((req, res) => {
-  const { pathname, searchParams } = new URL(req.url || '', `http://${req.headers.host}`);
-  const itemPath = searchParams.get('itemPath') || '';
+// const server = http.createServer((req, res) => {
+//   const { pathname, searchParams } = new URL(req.url || '', `http://${req.headers.host}`);
+//   const itemPath = searchParams.get('itemPath') || '';
 
-  // const safePath = path.join(safePath, itemPath);
+//   // const safePath = path.join(safePath, itemPath);
 
-  // 安全路径处理
-  const safeResolve = (baseDir: string, relPath: string) => {
-    return path.join(baseDir, path.normalize(relPath));
-  };
+//   // 安全路径处理
+//   const safeResolve = (baseDir: string, relPath: string) => {
+//     return path.join(baseDir, path.normalize(relPath));
+//   };
 
-  // /_apis/pipelines/workflows/:runId/artifacts
-  let pathMatch = pathname.match(/\/_apis\/pipelines\/workflows\/([^/]+)\/artifacts/);
-  if (pathMatch && req.method === 'GET') {
-    const runId = pathMatch[1];
-    const safePath = path.join(BASE_DIR, runId);
-    const dirContent = fs.readdirSync(safePath);
-    const fileList = dirContent.map((file) => {
-      return {
-        name: file,
-        fileContainerResourceUrl: `/download/${runId}?itemPath=${encodeURIComponent(file)}`,
-      };
-    });
+//   // /_apis/pipelines/workflows/:runId/artifacts
+//   let pathMatch = pathname.match(/\/_apis\/pipelines\/workflows\/([^/]+)\/artifacts/);
+//   if (pathMatch && req.method === 'GET') {
+//     const runId = pathMatch[1];
+//     const safePath = path.join(BASE_DIR, runId);
+//     const dirContent = fs.readdirSync(safePath);
+//     const fileList = dirContent.map((file) => {
+//       return {
+//         name: file,
+//         fileContainerResourceUrl: `/download/${runId}?itemPath=${encodeURIComponent(file)}`,
+//       };
+//     });
 
-    res.statusCode = 200;
-    res.end(JSON.stringify({ count: fileList.length, value: fileList }));
-  }
-  if (pathMatch && req.method === 'POST') {
-    const runId = pathMatch[1];
-    res.statusCode = 200;
-    res.end(JSON.stringify({ fileContainerResourceUrl: `/upload/${runId}` }));
-  }
-  if (pathMatch && req.method === 'PATCH') {
-    // const runId = pathMatch[1];
-    res.statusCode = 200;
-    res.end(JSON.stringify({ message: 'success' }));
-  }
+//     res.statusCode = 200;
+//     res.end(JSON.stringify({ count: fileList.length, value: fileList }));
+//   }
+//   if (pathMatch && req.method === 'POST') {
+//     const runId = pathMatch[1];
+//     res.statusCode = 200;
+//     res.end(JSON.stringify({ fileContainerResourceUrl: `/upload/${runId}` }));
+//   }
+//   if (pathMatch && req.method === 'PATCH') {
+//     // const runId = pathMatch[1];
+//     res.statusCode = 200;
+//     res.end(JSON.stringify({ message: 'success' }));
+//   }
 
-  // /upload/:runId
-  pathMatch = pathname.match(/\/upload\/([^/]+)/);
-  if (pathMatch && req.method === 'PUT') {
-    const runId = pathMatch[1];
-    const safePath = path.join(BASE_DIR, runId);
-    const gzip = req.headers['content-encoding'] === 'gzip';
-    const itemPathWithGzip = gzip ? `${itemPath}.gz__` : itemPath;
-    const filePath = safeResolve(safePath, itemPathWithGzip);
+//   // /upload/:runId
+//   pathMatch = pathname.match(/\/upload\/([^/]+)/);
+//   if (pathMatch && req.method === 'PUT') {
+//     const runId = pathMatch[1];
+//     const safePath = path.join(BASE_DIR, runId);
+//     const gzip = req.headers['content-encoding'] === 'gzip';
+//     const itemPathWithGzip = gzip ? `${itemPath}.gz__` : itemPath;
+//     const filePath = safeResolve(safePath, itemPathWithGzip);
 
-    // 创建目录
-    fs.mkdirSync(path.dirname(filePath), { recursive: true });
+//     // 创建目录
+//     fs.mkdirSync(path.dirname(filePath), { recursive: true });
 
-    // 写入文件
-    const fileWriteStream = fs.createWriteStream(filePath);
-    req.pipe(fileWriteStream);
+//     // 写入文件
+//     const fileWriteStream = fs.createWriteStream(filePath);
+//     req.pipe(fileWriteStream);
 
-    // 响应上传成功
-    fileWriteStream.on('close', () => {
-      res.statusCode = 200;
-      res.end(JSON.stringify({ message: 'success' }));
-    });
-  }
+//     // 响应上传成功
+//     fileWriteStream.on('close', () => {
+//       res.statusCode = 200;
+//       res.end(JSON.stringify({ message: 'success' }));
+//     });
+//   }
 
-  // /download/:container
-  pathMatch = pathname.match(/\/download\/([^/]+)/);
-  if (pathMatch && req.method === 'GET') {
-    const container = pathMatch[1];
-    const safePath = path.join(BASE_DIR, container, itemPath);
+//   // /download/:container
+//   pathMatch = pathname.match(/\/download\/([^/]+)/);
+//   if (pathMatch && req.method === 'GET') {
+//     const container = pathMatch[1];
+//     const safePath = path.join(BASE_DIR, container, itemPath);
 
-    const files: { path: string; itemType: string; contentLocation: string; }[] = [];
+//     const files: { path: string; itemType: string; contentLocation: string; }[] = [];
 
-    fs.readdirSync(safePath, { withFileTypes: true }).forEach((file) => {
-      if (file.isDirectory()) {
-        const filePath = path.join(safePath, file.name);
-        const relPath = path.relative(safePath, filePath);
-        const cleanRelPath = relPath.endsWith('.gz__') ? relPath.slice(0, -5) : relPath;
-        const contentLocation = `http://${req.headers.host}:${PORT}/artifact/${container}/${itemPath}/${cleanRelPath}`;
-        files.push({
-          path: path.posix.join(itemPath, relPath),
-          itemType: 'file',
-          contentLocation,
-        });
-      }
-    });
+//     fs.readdirSync(safePath, { withFileTypes: true }).forEach((file) => {
+//       if (file.isDirectory()) {
+//         const filePath = path.join(safePath, file.name);
+//         const relPath = path.relative(safePath, filePath);
+//         const cleanRelPath = relPath.endsWith('.gz__') ? relPath.slice(0, -5) : relPath;
+//         const contentLocation = `http://${req.headers.host}:${PORT}/artifact/${container}/${itemPath}/${cleanRelPath}`;
+//         files.push({
+//           path: path.posix.join(itemPath, relPath),
+//           itemType: 'file',
+//           contentLocation,
+//         });
+//       }
+//     });
 
-    console.log('files', files);
+//     console.log('files', files);
 
-    res.end(JSON.stringify({
-      value: files,
-    }));
-  }
+//     res.end(JSON.stringify({
+//       value: files,
+//     }));
+//   }
 
-  // /artifact/*path
-  pathMatch = pathname.match(/\/artifact\/([^/]+)/);
-  if (pathMatch && req.method === 'GET') {
-    const artifactPath = pathMatch[1];
-    const safePath = path.join(BASE_DIR, artifactPath);
+//   // /artifact/*path
+//   pathMatch = pathname.match(/\/artifact\/([^/]+)/);
+//   if (pathMatch && req.method === 'GET') {
+//     const artifactPath = pathMatch[1];
+//     const safePath = path.join(BASE_DIR, artifactPath);
 
-    // 检查文件是否存在
-    fs.access(safePath, fs.constants.F_OK, (err) => {
-      if (!err) {
-      // 文件存在，设置Content-Encoding为gzip（如果需要）
-        const isGzip = path.basename(safePath).endsWith('.gz__');
-        if (isGzip) {
-          res.setHeader('Content-Encoding', 'gzip');
-        }
+//     // 检查文件是否存在
+//     fs.access(safePath, fs.constants.F_OK, (err) => {
+//       if (!err) {
+//       // 文件存在，设置Content-Encoding为gzip（如果需要）
+//         const isGzip = path.basename(safePath).endsWith('.gz__');
+//         if (isGzip) {
+//           res.setHeader('Content-Encoding', 'gzip');
+//         }
 
-        // 传输文件流
-        const fileStream = fs.createReadStream(safePath);
-        fileStream.pipe(res);
+//         // 传输文件流
+//         const fileStream = fs.createReadStream(safePath);
+//         fileStream.pipe(res);
 
-        fileStream.on('error', () => {
-        // 如果文件流发生错误，返回404
-          res.statusCode = 404;
-          res.end('File not found');
-        });
-      } else {
-      // 文件不存在，返回404
-        res.statusCode = 404;
-        res.end('File not found');
-      }
-    });
-  }
-});
+//         fileStream.on('error', () => {
+//         // 如果文件流发生错误，返回404
+//           res.statusCode = 404;
+//           res.end('File not found');
+//         });
+//       } else {
+//       // 文件不存在，返回404
+//         res.statusCode = 404;
+//         res.end('File not found');
+//       }
+//     });
+//   }
+// });
 
-// 启动服务器
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+// // 启动服务器
+// server.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}`);
+// });
