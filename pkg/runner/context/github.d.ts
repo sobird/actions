e46fd4d2b16a2328022f7fee6ddf1b506cb40c7d
sobird <i@sobird.me>
2024-05-28 09:46:50 +0800

    chore: update context github

--- pkg/runner/context/github.d.ts
+++ pkg/runner/context/github.d.ts
@@ -1,133 +1,142 @@
 /**
- * 工作流程中任何作业或步骤期间可用的顶层上下文。
+ * The top-level context available during any job or step in a workflow.
  *
- * `github` 上下文包含有关工作流运行和触发运行的事件的信息。 还可以读取环境变量中的大多数 `github` 上下文数据。
+ * The github context contains information about the workflow run and the event that triggered the run.
+ * You can also read most of the github context data in environment variables. For more information about environment variables, see "{@link https://docs.github.com/en/actions/learn-github-actions/variables Variables}."
+ *
+ * Warning: When using the whole github context, be mindful that it includes sensitive information such as github.token.
+ * GitHub masks secrets when they are printed to the console, but you should be cautious when exporting or printing the context.
+ *
+ * Warning: When creating workflows and actions, you should always consider whether your code might execute untrusted input from possible attackers.
+ * Certain contexts should be treated as untrusted input, as an attacker could insert their own malicious content. For more information, see "{@link https://docs.github.com/en/actions/security-guides/security-hardening-for-github-actions#understanding-the-risk-of-script-injections Security hardening for GitHub Actions}."
  */
 export interface Github {
   /**
-   * 正在运行的操作的名称，或步骤的 id。
+   * The name of the action currently running, or the id of a step.
    *
-   * 在当前步骤运行脚本（无 id）时，GitHub 删除特殊字符并使用名称 __run。
-   * 如果在同一作业中多次使用相同的操作，则名称将包含一个前面跟序号和下划线的后缀。
-   * 例如，运行的第一个脚本名称 __run，则第二个脚本将命名为 __run_2。
-   * 同样，actions/checkout 的第二次调用将为 actionscheckout2。
+   * GitHub removes special characters, and uses the name __run when the current step runs a script without an id.
+   * If you use the same action more than once in the same job, the name will include a suffix with the sequence number with underscore before it.
+   * For example, the first script you run will have the name __run, and the second script will be named __run_2. Similarly,
+   * the second invocation of actions/checkout will be actionscheckout2.
    */
   action: string;
   /**
-   * 操作所在的路径。
+   * The path where an action is located. This property is only supported in composite actions.
    *
-   * 此属性仅在复合操作中受支持。可以使用此路径访问与操作位于同一存储库中的文件，例如将目录更改为该路径： `cd ${{ github.action_path }}` 。
+   * You can use this path to access files located in the same repository as the action, for example by changing directories to the path: cd ${{ github.action_path }} .
    */
   action_path: string;
   /**
-   * 对于执行操作的步骤，这是正在执行的操作的引用。 例如 `v2`。
+   * For a step executing an action, this is the ref of the action being executed. For example, v2.
    *
-   * 不要在 `run` 关键字中使用。 若要使此上下文适用于复合操作，请在复合操作的 `env` 上下文中引用它。
+   * Do not use in the run keyword.
+   * To make this context work with composite actions, reference it within the env context of the composite action.
    */
   action_ref: string;
   /**
-   * 对于执行操作的步骤，这是操作的所有者和存储库名称。 例如 `actions/checkout`。
+   * For a step executing an action, this is the owner and repository name of the action. For example, actions/checkout.
    *
-   * 不要在 `run` 关键字中使用。 若要使此上下文适用于复合操作，请在复合操作的 `env` 上下文中引用它。
+   * Do not use in the run keyword.
+   * To make this context work with composite actions, reference it within the env context of the composite action.
    */
   action_repository: string;
-
   /**
-   * 对于复合操作，这是复合操作的当前结果。
+   * For a composite action, the current result of the composite action.
    */
   action_status: string;
   /**
-   * 触发初始工作流运行的用户的用户名。
+   * The username of the user that triggered the initial workflow run.
    *
-   * 如果工作流运行是重新运行，则此值可能与 github.triggering_actor 不同。
-   * 即使启动重新运行的参与者 (github.triggering_actor) 具有不同的权限，任何工作流重新运行都将使用 github.actor 的权限。
+   * If the workflow run is a re-run, this value may differ from github.triggering_actor.
+   * Any workflow re-runs will use the privileges of github.actor,
+   * even if the actor initiating the re-run (github.triggering_actor) has different privileges.
    */
   actor: string;
   /**
-   * 触发初始工作流运行的个人或应用的帐户 ID。 例如 1234567。 请注意，这与参与者用户名不同。
+   * The account ID of the person or app that triggered the initial workflow run. For example, 1234567.
+   *
+   * Note that this is different from the actor username.
    */
   actor_id: string;
-
   /**
-   * GitHub REST API 的 URL。
+   * The URL of the GitHub REST API.
    */
   api_url: string;
-
   /**
-   * 工作流运行中拉取请求的 base_ref 或目标分支。
+   * The base_ref or target branch of the pull request in a workflow run.
    *
-   * 仅当触发工作流运行的事件是 pull_request 或 pull_request_target 时，才可使用此属性。
+   * This property is only available when the event that triggers a workflow run is either pull_request or pull_request_target.
    */
   base_ref: string;
   /**
-   * 运行器上从工作流命令到设置环境变量的文件路径。
+   * Path on the runner to the file that sets environment variables from workflow commands.
    *
-   * 此文件对于当前步骤是唯一的，并且是作业中每个步骤的不同文件。
-   * 有关详细信息，请参阅“{@link https://docs.github.com/zh/actions/using-workflows/workflow-commands-for-github-actions#setting-an-environment-variable GitHub Actions 的工作流命令}”。
+   * This file is unique to the current step and is a different file for each step in a job.
+   * For more information, see "{@link https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-environment-variable Workflow commands for GitHub Actions}."
    */
   env: string;
   /**
-   * 完整事件 Webhook 有效负载。
+   * The full event webhook payload.
    *
-   * 您可以使用上下文访问事件的个别属性。
-   * 此对象与触发工作流运行的事件的 web 挂钩有效负载相同，并且对于每个事件都是不同的。
-   * 每个 GitHub Actions 事件的 Webhook 在“{@link https://docs.github.com/zh/actions/using-workflows/events-that-trigger-workflows 触发工作流的事件}”中链接。
-   * 例如，对于由 {@link https://docs.github.com/zh/actions/using-workflows/events-that-trigger-workflows#push push 事件}触发的工作流运行，此对象包含推送 {@link https://docs.github.com/zh/webhooks-and-events/webhooks/webhook-events-and-payloads#push webhook 有效负载}的内容。
+   * You can access individual properties of the event using this context.
+   * This object is identical to the webhook payload of the event that triggered the workflow run, and is different for each event.
+   * The webhooks for each GitHub Actions event is linked in "{@link https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows Events that trigger workflows}."
+   * For example, for a workflow run triggered by the {@link https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows#push `push` event}, this object contains the contents of the {@link https://docs.github.com/en/webhooks-and-events/webhooks/webhook-events-and-payloads#push push webhook payload}.
    */
   event: object;
   /**
-   * 触发工作流运行的事件的名称。
+   * The name of the event that triggered the workflow run.
    */
   event_name: string;
   /**
-   * 运行器上包含完整事件 Webhook 有效负载的文件的路径。
+   * The path to the file on the runner that contains the full event webhook payload.
    */
   event_path: string;
   /**
-   * GitHub GraphQL API 的 URL。
+   * The URL of the GitHub GraphQL API.
    */
   graphql_url: string;
   /**
-   * 工作流运行中拉取请求的 `head_ref` 或源分支。
+   * The head_ref or source branch of the pull request in a workflow run.
    *
-   * 仅当触发工作流运行的事件是 `pull_request` 或 `pull_request_target` 时，才可使用此属性。
+   * This property is only available when the event that triggers a workflow run is either pull_request or pull_request_target.
    */
   head_ref: string;
   /**
-   * 当前作业的 job_id。
+   * The {@link https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_id `job_id`} of the current job.
    *
-   * 注意：此上下文属性是由 Actions 运行程序设置的，仅在作业的执行 steps 中可用。 否则，此属性的值将为 null。
+   * Note: This context property is set by the Actions runner, and is only available within the execution steps of a job. Otherwise, the value of this property will be null.
    */
   job: string;
   /**
-   * 运行器上从工作流命令设置系统 PATH 变量的文件的路径。
+   * Path on the runner to the file that sets system PATH variables from workflow commands.
    *
-   * 此文件对于当前步骤是唯一的，并且是作业中每个步骤的不同文件。
-   * 有关详细信息，请参阅“{@link https://docs.github.com/zh/actions/using-workflows/workflow-commands-for-github-actions#adding-a-system-path GitHub Actions 的工作流命令}”。
+   * This file is unique to the current step and is a different file for each step in a job.
+   * For more information, see "{@link https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#adding-a-system-path Workflow commands for GitHub Actions}."
    */
   path: string;
   /**
-   * 触发工作流运行的分支或标记的格式完整的参考。
+   * The fully-formed ref of the branch or tag that triggered the workflow run.
    *
-   * 对于 `push` 触发的工作流，这是推送的分支或标记参考。
-   * 对于 `pull_request` 触发的工作流，这是拉取请求合并分支。
-   * 对于 `release` 触发的工作流，这是创建的发布标记。
-   * 对于其他触发器，这是触发工作流运行的分支或标记参考。
-   * 此变量仅在分支或标记可用于事件类型时才会设置。
-   * 给定的参考格式完整，这意味着对于分支，其格式为 `refs/heads/<branch_name>`，
-   * 对于拉取请求，其格式为 `refs/pull/<pr_number>/merge`，对于标签，其格式为 `refs/tags/<tag_name>`。
-   * 例如，`refs/heads/feature-branch-1`。
+   * For workflows triggered by `push`, this is the branch or tag ref that was pushed.
+   * For workflows triggered by `pull_request`, this is the pull request merge branch.
+   * For workflows triggered by `release`, this is the release tag created.
+   * For other triggers, this is the branch or tag ref that triggered the workflow run.
+   * This is only set if a branch or tag is available for the event type.
+   * The ref given is fully-formed, meaning that for branches the format is `refs/heads/<branch_name>`, for pull requests it is `refs/pull/<pr_number>/merge`, and for tags it is `refs/tags/<tag_name>`.
+   * For example, `refs/heads/feature-branch-1`.
    */
   ref: string;
   /**
-   * 触发工作流运行的分支或标记的短参考名称。
+   * The short ref name of the branch or tag that triggered the workflow run.
+   * This value matches the branch or tag name shown on GitHub.
+   * For example, `feature-branch-1`.
    *
-   * 此值与 `GitHub` 上显示的分支或标记名称匹配。 例如 `feature-branch-1`。
-   * 拉取请求的格式为 `<pr_number>/merge`。
+   * For pull requests, the format is `<pr_number>/merge`.
    */
   ref_name: string;
   /**
-   * 如果为触发工作流运行的 ref 配置分支保护 或 规则集 ，则为 true。
+   * `true` if branch protections or {@link https://docs.github.com/en/repositories/configuring-branches-and-merges-in-your-repository/managing-rulesets/managing-rulesets-for-a-repository rulesets} are configured for the ref that triggered the workflow run.
    */
   ref_protected: boolean;
   /**
@@ -135,97 +144,96 @@ export interface Github {
    */
   ref_type: 'branch' | 'tag';
   /**
-   * 所有者和存储库名称。 例如，octocat/Hello-World。
+   * The owner and repository name. For example, `octocat/Hello-World`.
    */
   repository: string;
   /**
-   * 存储库的 ID。 例如 123456789。 请注意，这与存储库名称不同。
+   * The ID of the repository. For example, `123456789`. Note that this is different from the repository name.
    */
   repository_id: string;
   /**
-   * 存储库所有者的用户名。 例如 octocat。
+   * The repository owner's username. For example, `octocat`.
    */
   repository_owner: string;
   /**
-   * 存储库所有者的帐户 ID。 例如 1234567。 请注意，这与所有者名称不同。
+   * The repository owner's account ID. For example, `1234567`. Note that this is different from the owner's name.
    */
   repository_owner_id: string;
   /**
-   * 存储库的 Git URL。 例如，`git://github.com/octocat/hello-world.git`。
+   * The Git URL to the repository. For example, `git://github.com/octocat/hello-world.git`.
    */
   repositoryUrl: string;
   /**
-   * 工作流运行日志和项目保留的天数。
+   * The number of days that workflow run logs and artifacts are kept.
    */
   retention_days: string;
 
   /**
-   * 存储库中每个工作流运行的唯一编号。
-   *
-   * 如果您重新执行工作流程运行，此编号不变。
+   * A unique number for each workflow run within a repository.
+   * This number does not change if you re-run the workflow run.
    */
   run_id: string;
   /**
-   * 仓库中特定工作流程每个运行的唯一编号。
-   *
-   * 工作流首次运行时，此编号从 1 开始，并随着每次新的运行而递增。 如果您重新执行工作流程运行，此编号不变。
+   * A unique number for each run of a particular workflow in a repository.
+   * This number begins at 1 for the workflow's first run, and increments with each new run.
+   * This number does not change if you re-run the workflow run.
    */
   run_number: string;
   /**
-   * 存储库中特定工作流运行的每次尝试的唯一编号。
-   *
-   * 对于工作流程运行的第一次尝试，此数字从 1 开始，并随着每次重新运行而递增。
+   * A unique number for each attempt of a particular workflow run in a repository.
+   * This number begins at 1 for the workflow run's first attempt, and increments with each re-run.
    */
   run_attempt: string;
   /**
-   * 工作流中使用的机密的来源。
-   *
-   * 可能的值为 None、Actions、Codespaces 或 Dependabot。
+   * The source of a secret used in a workflow. Possible values are `None`, `Actions`, `Codespaces`, or `Dependabot`.
    */
   secret_source: 'None' | 'Actions' | 'Codespaces' | 'Dependabot';
   /**
-   * GitHub 服务器的 URL。 例如：https://github.com。
+   * The URL of the GitHub server. For example: `https://github.com`.
    */
   server_url: string;
   /**
-   * 触发工作流的提交 SHA。
+   * The commit SHA that triggered the workflow.
    *
-   * 此提交 SHA 的值取决于触发工作流程的事件。 有关详细信息，请参阅“{@link https://docs.github.com/zh/actions/using-workflows/events-that-trigger-workflows 触发工作流的事件}”。
-   * 例如，ffac537e6cbbf934b08745a378932722df287a53。
+   * The value of this commit SHA depends on the event that triggered the workflow.
+   * For more information, see "{@link https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows Events that trigger workflows}."
+   * For example, `ffac537e6cbbf934b08745a378932722df287a53`.
    */
   sha: string;
   /**
-   * 用于代表存储库中安装的 GitHub 应用程序进行身份验证的令牌。
+   * A token to authenticate on behalf of the GitHub App installed on your repository.
+   * This is functionally equivalent to the GITHUB_TOKEN secret.
+   * For more information, see "{@link https://docs.github.com/en/actions/security-guides/automatic-token-authentication Automatic token authentication}."
    *
-   * 在功能上，这与 `GITHUB_TOKEN` 机密等效。 有关详细信息，请参阅“{@link https://docs.github.com/zh/actions/security-guides/automatic-token-authentication 自动令牌身份验证}”。
-   * 注意：此上下文属性是由 Actions 运行程序设置的，仅在作业的执行 steps 中可用。 否则，此属性的值将为 null。
+   * Note: This context property is set by the Actions runner, and is only available within the execution steps of a job.
+   * Otherwise, the value of this property will be null.
    */
   token: string;
   /**
-   * 发起工作流运行的用户的用户名。
+   * The username of the user that initiated the workflow run.
    *
-   * 如果工作流运行是重新运行，则此值可能与 `github.actor` 不同。
-   * 即使启动重新运行的参与者 (`github.triggering_actor`) 具有不同的权限，任何工作流重新运行都将使用 `github.actor` 的权限。
+   * If the workflow run is a re-run, this value may differ from `github.actor`.
+   * Any workflow re-runs will use the privileges of `github.actor`, even if the actor initiating the re-run (`github.triggering_actor`) has different privileges.
    */
   triggering_actor: string;
   /**
-   * 工作流的名称。
+   * The name of the workflow.
    *
-   * 如果工作流程文件未指定 name，则此属性的值是存储库中工作流程文件的完整路径。
+   * If the workflow file doesn't specify a name, the value of this property is the full path of the workflow file in the repository.
    */
   workflow: string;
   /**
-   * 工作流的引用路径。
+   * The ref path to the workflow.
    *
-   * 例如，`octocat/hello-world/.github/workflows/my-workflow.yml@refs/heads/my_branch`。
+   * For example, `octocat/hello-world/.github/workflows/my-workflow.yml@refs/heads/my_branch`.
    */
   workflow_ref: string;
   /**
-   * 工作流文件的提交 SHA
+   * The commit SHA for the workflow file.
    */
   workflow_sha: string;
   /**
-   * 运行器上步骤的默认工作目录，以及使用 checkout 操作时存储库的默认位置。
+   * The default working directory on the runner for steps, and the default location of your repository when using the {@link https://github.com/actions/checkout checkout} action.
    */
   workspace: string;
 }
