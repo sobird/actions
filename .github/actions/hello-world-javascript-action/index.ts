55ada3f3a061158d0b7d3cb06b5659a8bea3ee04
sobird <i@sobird.me>
2024-06-23 16:43:39 +0800

    chore: hello-world-javascript-action support ts @vercel/ncc

--- .github/actions/hello-world-javascript-action/index.ts
+++ .github/actions/hello-world-javascript-action/index.ts
@@ -11,5 +11,5 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed((error as Error).message);
 }
