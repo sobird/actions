46dccd1a96d98eff569d69b2dabd33ee64ce5191
sobird <i@sobird.me>
2024-07-05 12:45:28 +0800

    test: workflow commands test

--- test.ts
+++ test.ts
@@ -1,21 +1,110 @@
-import fs from 'node:fs';
+import * as os from 'os';
 
-import LineWritable from './pkg/common/line-writable';
+import * as platform from '@actions/core';
 
-const lines: string[] = [];
+// For internal use, subject to change.
 
-const lineWriter = new LineWritable();
-lineWriter.on('line', (line: string) => {
-  console.log('line', line);
-  lines.push(line);
-});
+// We use any as a valid input type
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-lineWriter.on('finish', () => {
-  // console.log('lines', lines);
-});
+export interface CommandProperties {
+  [key: string]: any
+}
 
-const rs = fs.createReadStream('package.json', {
-  highWaterMark: 100,
-});
+export function toCommandValue(input: any): string {
+  if (input === null || input === undefined) {
+    return '';
+  } if (typeof input === 'string' || input instanceof String) {
+    return input as string;
+  }
+  return JSON.stringify(input);
+}
 
-rs.pipe(lineWriter);
+function escapeData(s: any): string {
+  return toCommandValue(s)
+    .replace(/%/g, '%25')
+    .replace(/\r/g, '%0D')
+    .replace(/\n/g, '%0A');
+}
+
+function escapeProperty(s: any): string {
+  return toCommandValue(s)
+    .replace(/%/g, '%25')
+    .replace(/\r/g, '%0D')
+    .replace(/\n/g, '%0A')
+    .replace(/:/g, '%3A')
+    .replace(/,/g, '%2C');
+}
+
+const CMD_STRING = '::';
+
+class Command {
+  private readonly command: string;
+
+  private readonly message: string;
+
+  private readonly properties: CommandProperties;
+
+  constructor(command: string, properties: CommandProperties, message: string) {
+    if (!command) {
+      command = 'missing.command';
+    }
+
+    this.command = command;
+    this.properties = properties;
+    this.message = message;
+  }
+
+  toString(): string {
+    let cmdStr = CMD_STRING + this.command;
+
+    if (this.properties && Object.keys(this.properties).length > 0) {
+      cmdStr += ' ';
+      let first = true;
+      for (const key in this.properties) {
+        if (this.properties.hasOwnProperty(key)) {
+          const val = this.properties[key];
+          if (val) {
+            if (first) {
+              first = false;
+            } else {
+              cmdStr += ',';
+            }
+
+            cmdStr += `${key}=${escapeProperty(val)}`;
+          }
+        }
+      }
+    }
+
+    cmdStr += `${CMD_STRING}${escapeData(this.message)}`;
+    return cmdStr;
+  }
+}
+
+/**
+ * Commands
+ *
+ * Command Format:
+ *   ::name key=value,key=value::message
+ *
+ * Examples:
+ *   ::warning::This is the message
+ *   ::set-env name=MY_VAR::some value
+ */
+export function issueCommand(
+  command: string,
+  properties: CommandProperties,
+  message: any,
+): void {
+  const cmd = new Command(command, properties, message);
+  process.stdout.write(cmd.toString() + os.EOL);
+}
+
+export function issue(name: string, message = ''): void {
+  issueCommand(name, {}, message);
+}
+
+issueCommand('set-env', { name: 'sobird' }, 'hello world');
+issueCommand('add-mask', {}, 'secert');
+console.log('platform', platform.platform);
