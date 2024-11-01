9b0de826a2a2fb705147d67ce01f5b667b074903
sobird <i@sobird.me>
2024-11-01 21:39:49 +0800

    chore: test docker hub action

--- pkg/runner/action/factory/docker.ts
+++ pkg/runner/action/factory/docker.ts
@@ -1,12 +1,17 @@
 import path from 'node:path';
 
+import log4js from 'log4js';
+import shellQuote from 'shell-quote';
 import * as tar from 'tar';
 
 import Executor from '@/pkg/common/executor';
 import docker from '@/pkg/docker';
+import Runner from '@/pkg/runner';
 import { createSafeName } from '@/utils';
 
 import Action from '..';
+
+const logger = log4js.getLogger();
 
 class DockerAction extends Action {
   protected main() {
@@ -26,47 +31,93 @@ class DockerAction extends Action {
           const platform = `${imageInspect.Os}/${imageInspect.Architecture}`;
 
           if ((!runner.config.containerPlatform || runner.config.containerPlatform === platform) && !runner.config.rebuild) {
-            return;
+            logger.debug("image '%s' for architecture '%s' already exists", image, runner.config.containerPlatform || '');
+          } else {
+            await dockerImage.remove();
+            throw new Error('image need build');
+          }
+        } catch (error) {
+          // need build image
+          let archive = new tar.Pack() as unknown as NodeJS.ReadableStream;
+          try {
+            archive = await runner.container!.getArchive(`${this.Dir}`);
+          } catch (err) {
+            if (runner.config.actionCache) {
+              await runner.config.actionCache.fetch(uses.repositoryUrl, uses.repository, uses.ref);
+              archive = await runner.config.actionCache.archive(uses.repository, uses.ref, uses.path);
+              actionPath = uses.path;
+            }
           }
 
-          await dockerImage.remove();
-        } catch (error) {
-          //
-        }
+          const parser = new tar.Parser({ strip: 1 });
+          const pack = new tar.Pack();
+          archive.pipe(parser);
 
-        let archive = new tar.Pack() as unknown as NodeJS.ReadableStream;
-        try {
-          archive = await runner.container!.getArchive(`${this.Dir}`);
-        } catch (error) {
-          if (runner.config.actionCache) {
-            await runner.config.actionCache.fetch(uses.repositoryUrl, uses.repository, uses.ref);
-            archive = await runner.config.actionCache.archive(uses.repository, uses.ref, uses.path);
-            actionPath = uses.path;
-          }
-        }
-
-        const parser = new tar.Parser({ strip: 1 });
-        const pack = new tar.Pack();
-        archive.pipe(parser);
-
-        parser.on('entry', (entry: tar.ReadEntry) => {
+          parser.on('entry', (entry: tar.ReadEntry) => {
           // strip action dir
           // eslint-disable-next-line no-param-reassign
-          entry.path = path.relative(actionPath, entry.path);
-          pack.add(entry);
-        });
-        parser.on('finish', () => {
-          pack.end();
-        });
+            entry.path = path.relative(actionPath, entry.path);
+            pack.add(entry);
+          });
+          parser.on('finish', () => {
+            pack.end();
+          });
 
-        const stream = await docker.buildImage(pack as unknown as NodeJS.ReadableStream, { t: image });
-        await new Promise((resolve, reject) => {
-          docker.modem.followProgress(stream, (err, res) => { return (err ? reject(err) : resolve(res)); });
-        });
+          const stream = await docker.buildImage(pack as unknown as NodeJS.ReadableStream, { t: image });
+          await new Promise((resolve, reject) => {
+            docker.modem.followProgress(stream, (err, res) => { return (err ? reject(err) : resolve(res)); });
+          });
+        }
       } else {
         image = image.substring('docker://'.length);
       }
+
+      const stepWith = stepAction?.with.evaluate(runner);
+      let args = shellQuote.parse(stepWith?.args || '');
+      if (!args) {
+        args = this.runs.args;
+        // todo
+      }
+
+      const { entrypoint } = stepWith;
+
+      if (!entrypoint) {
+        // todo
+      }
     });
+  }
+
+  applyArgs(runner: Runner) {
+    const rc = runner;
+    const stepModel = runner.stepAction;
+
+    const inputs: Record<string, string> = {};
+    // Set Defaults
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
+    for (const k in this.inputs) {
+      inputs[k] = this.inputs[k].default.evaluate(runner);
+    }
+
+    if (stepModel?.with) {
+      // eslint-disable-next-line no-restricted-syntax, guard-for-in
+      for (const k in stepModel.with) {
+        inputs[k] = stepModel.with.evaluate(runner)?.[k] || '';
+      }
+    }
+
+    // mergeIntoMap(step, step.getEnv(), inputs);
+
+    // const stepEE = rc.newStepExpressionEvaluator(ctx, step);
+    // for (let i = 0; i < cmd.length; i++) {
+    //   cmd[i] = await stepEE.interpolate(ctx, cmd[i]);
+    // }
+
+    // mergeIntoMap(step, step.getEnv(), this.runs.env);
+
+    // const ee = rc.newStepExpressionEvaluator(ctx, step);
+    // for (const k in step.getEnv()) {
+    //   (step.getEnv())[k] = await ee.interpolate(ctx, (step.getEnv())[k]);
+    // }
   }
 
   public static IsDockerfile(image: string) {
--- pkg/workflow/job/step/action/docker.ts
+++ pkg/workflow/job/step/action/docker.ts
@@ -1,18 +1,64 @@
+import shellQuote from 'shell-quote';
+
 import Executor from '@/pkg/common/executor';
+import Runner from '@/pkg/runner';
+import DockerContainer from '@/pkg/runner/container/docker';
 
 import StepAction from '.';
 
 class StepActionDocker extends StepAction {
-  public pre() {
-    return new Executor(() => {});
-  }
-
   public main() {
-    return new Executor(() => {});
+    return this.executor(new Executor(async (ctx) => {
+      const runner = ctx!;
+      const image = this.uses.toString().substring('docker://'.length);
+      const stepWith = this.with.evaluate(runner);
+      const cmd = shellQuote.parse(stepWith?.args || '');
+
+      let entrypoint: string[] = [];
+
+      if (stepWith?.entrypoint) {
+        entrypoint = [stepWith.entrypoint];
+      }
+
+      console.log('cmd', cmd, entrypoint);
+
+      const container = this.Container(runner, image, cmd, entrypoint);
+
+      return Executor.Pipeline(
+        // runner.pullServicesImage(),
+        container.start(),
+      );
+    }));
   }
 
-  public post() {
-    return new Executor(() => {});
+  Container(runner: Runner, image: string, cmd: string[], entrypoint: string[]) {
+    const { environment } = this;
+
+    const { config } = runner;
+    const [binds, mounts] = runner.BindsAndMounts;
+    const credentials = runner.Credentials;
+
+    const name = runner.ContainerName(runner.context.github.action);
+
+    return new DockerContainer({
+      name,
+      image,
+      env: environment,
+      cmd,
+      entrypoint,
+      workdir: config.workdir,
+      authconfig: {
+        ...credentials,
+      },
+      binds,
+      mounts,
+      networkMode: `container:${runner.ContainerName()}`,
+      privileged: config.containerPrivileged,
+      usernsMode: config.containerUsernsMode,
+      platform: config.containerPlatform,
+      capAdd: config.containerCapAdd,
+      capDrop: config.containerCapDrop,
+    }, config.workspace);
   }
 }
 
