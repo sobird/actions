5c87f75b1bcac0d0c1dce98185c35c2599f67127
sobird <i@sobird.me>
2024-11-26 20:42:00 +0800

    chore: service runner method declare impled

--- services/runner/declare.ts
+++ services/runner/declare.ts
@@ -1,31 +1,29 @@
 import { ConnectError } from '@connectrpc/connect';
 
-import { ActionsRunnerModel } from '@/models';
-import Constants from '@/pkg/common/constants';
-import { DeclareResponse } from '@/pkg/service/runner/v1/messages_pb';
+import { DeclareResponse, Runner } from '@/pkg/service/runner/v1/messages_pb';
 
-import type { RunnerServiceMethodImpl } from '.';
+import type { ServiceMethodImpl } from '.';
+import { RunnerModelFrom } from './interceptors/with_runner';
 
-const { XRunnerUUID, XRunnerToken, XRunnerVersion } = Constants.Protocol;
+export const declare: ServiceMethodImpl<'declare'> = async (req, { values }) => {
+  const runner = RunnerModelFrom(values)!;
+  runner.labels = req.labels;
+  runner.version = req.version;
 
-export const declare: RunnerServiceMethodImpl<'declare'> = async (req, { requestHeader }) => {
-  console.log('requestHeader', requestHeader);
-  const runnerUUID = requestHeader.get(XRunnerUUID);
-  console.log('runnerUUID', runnerUUID);
-
-  const result = await ActionsRunnerModel.update({
-    labels: req.labels,
-    version: req.version,
-  }, {
-    where: {
-      uuid: 'dd5e1e50-33d4-4e5d-91b8-ddc2a1a022e5'!,
-    },
-    returning: true,
-  });
-
-  console.log('result', result);
+  try {
+    await runner.save();
+  } catch (error) {
+    throw new ConnectError(`update runner: ${(error as Error).message}`, 13);
+  }
 
   return new DeclareResponse({
-
+    runner: new Runner({
+      id: runner.id,
+      uuid: runner.uuid,
+      token: runner.token,
+      name: runner.name,
+      version: runner.version,
+      labels: runner.labels,
+    }),
   });
 };
