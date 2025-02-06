2ac5df91aa8c1a4c888d238c8f9deae19f616e41
sobird <i@sobird.me>
2025-02-06 14:25:15 +0800

    fix: pnpm log4js Module not found

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['sequelize'],
+  serverExternalPackages: ['sequelize', 'log4js'],
   /* config options here */
 };
 
