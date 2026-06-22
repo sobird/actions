49bd30d4d0f3d798f979c940330c2329d134c756
sobird <i@sobird.me>
2026-06-22 16:05:16 +0800

    chore: update withTimeout

--- src/utils/withTimeout.ts
+++ src/utils/withTimeout.ts
@@ -1,6 +1,10 @@
 import { setTimeout } from 'node:timers/promises';
 
-export async function withTimeout<T>(awaited: Promise<T> | T, ms?: number, message: string = 'Operation timed out') {
+export async function withTimeout<T>(
+  awaited: Promise<T> | T,
+  ms: number = 1000,
+  message: string = `Operation timed out after ${ms}ms`,
+) {
   const ac = new AbortController();
   const signal = ac.signal;
 
